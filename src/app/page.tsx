3e8b7e90942fbac838b4811228c49edeb889cf67
nekn3x <nekn3x@gmail.com>
2024-10-07 18:46:30 +0900

    docker-compose.ymlを修正
    node_modulesをローカルマシンと共通にする
    
    a
    
    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,20 @@ import dayjs from "dayjs";
 import styles from "./index.module.css";
 
 export default async function Home() {
-  // const hello = await api.healthcheck();
-  // const session = await getServerAuthSession();
-
-  // void api.post.getLatest.prefetch();
-  const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  const createOrder = await api.purchase.createOrder({
+    supplierName: "サプライヤーA",
+    deliveryDate: dayjs().add(1, "day").format("YYYY-MM-DD"),
+    details: [
+      { flowerId: 1, orderUnit: 1 },
+      { flowerId: 2, orderUnit: 2 },
+    ],
+  });
+  const session = await getServerAuthSession();
 
   return (
     // <HydrateClient>
     <main className={styles.main}>
-      <div className={styles.container}>aa</div>
+      <div className={styles.container}></div>
     </main>
     // </HydrateClient>
   );
